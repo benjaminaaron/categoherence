701049b96b7e724b9b2d8c3b1750c410e6f63009
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2016-04-20 14:08:06 +0200

    serve session.html if something after / in URL

--- app.js
+++ app.js
@@ -7,7 +7,17 @@ app.get('/', function(req, res){
    res.sendFile(__dirname + '/public/index.html');
 });
 
+app.get('/:sessionId', function(req, res){   
+   console.log("sessionId requested: " + req.params.sessionId);
+   res.sendFile(__dirname + '/public/session.html');
+});
+
 io.on('connection', function(socket){
+   
+   socket.on('login', function(sessionId){ // emitted by session.html 
+      socket.emit('directmessage', 'welcome to session ' + sessionId + ', your id is ' + socket.id);
+   });
+   
    console.log('user connected: ' + socket.id);
    socket.on('disconnect', function(){
       console.log('user disconnected: ' + socket.id);
