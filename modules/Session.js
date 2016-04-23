1d72bec0f6af2fe5876eae2df3505a580fef21a0
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2016-04-23 19:21:20 +0200

    WIP store subgroups at submission

--- modules/Session.js
+++ modules/Session.js
@@ -2,8 +2,13 @@
 var Session = function(info, set) {
     this.info = info;
     this.info.entities  = {};
-    for(var i = 0; i < set.length; i ++)
-        this.info.entities['id' + i] = set[i];
+    
+    this.subgroups = {};
+    
+    for(var i = 0; i < set.length; i ++) {
+        this.info.entities[i] = set[i];
+        this.subgroups[i] = 0;
+    }
     
     this.submissions = [];
     
@@ -16,9 +21,28 @@ Session.prototype = {
         console.log(this.info);
         console.log('---------- end: ' + this.info.id);
     },
-    handleSubmission: function(submissionData) {
-        this.submissions.push(submissionData);
-        //TODO
+    sortIds: function(arr) {
+        if(arr.length == 1)
+            return arr[0];
+        else {
+            var sorted = arr.map(Number).sort(function(a,b){return a - b;});
+            var idStr = '';
+            for(var i = 0; i < sorted.length; i ++)
+                idStr += '-' + sorted[i];
+            return idStr.substring(1);
+        }
+    },
+    handleSubmission: function(data) {
+        this.submissions.push(data);
+        for(var i = 0; i < data.groupings.length; i ++) {
+            var grouping = data.groupings[i];
+            var idStr = this.sortIds(grouping);
+            if(this.subgroups[idStr])
+                this.subgroups[idStr] += 1;
+            else 
+                this.subgroups[idStr] = 1;
+        }
+        console.log(this.subgroups);
     },
     getResult: function() {
         var resultData = {
